189fc8b2a2276ff85973003534da3f774de5cf8a
Filip Strajnar <filip.strajnar@gmail.com>
2022-12-03 01:40:34 +0100

    More key generation options

--- lib/common/generate_key.ex
+++ lib/common/generate_key.ex
@@ -7,4 +7,13 @@ defmodule Common.GenerateKey do
     def key128bits do
         :crypto.strong_rand_bytes(16)
     end
+
+    def key2048bits do
+        :crypto.strong_rand_bytes(256)
+    end
+
+    def key4096bits do
+        :crypto.strong_rand_bytes(512)
+    end
+
 end
